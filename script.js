fbeb0877679c0d80a47fec74161966bc5f6a0df3
SWebDev55 <112745821+SWebDev55@users.noreply.github.com>
2024-02-23 15:58:16 +0700

    loto88

---
+++ script.js
@@ -0,0 +1,37 @@
+// random telegram
+function randomTlg() {
+    var telegram = [
+        'https://t.me/Nguyenminh132',
+        'https://t.me/Dangngockhanhhh',
+        'https://t.me/lylong1779',
+        'https://t.me/ngoclinh0972',
+        'https://t.me/daohuyen113',
+        'https://t.me/emtrangnehihi',
+        'https://t.me/suohu58',
+        'https://t.me/thuytrang6656',
+        'https://t.me/lenghinghi',
+        'https://t.me/thattuyet_999',
+        'https://t.me/thythy868',
+        'https://t.me/baonamhay88',
+        'https://t.me/huyvaobo',
+        'https://t.me/thanhpham19971',
+        'https://t.me/boosshoanganh',
+        'https://t.me/huynhhy9',
+        'https://t.me/mylovehtq',
+        'https://t.me/betram1123',
+        'https://t.me/zzmailyzz86'
+    ];
+    var i = parseInt(Math.random() * telegram.length);
+    location.href = telegram[i];
+}
+
+
+// random zalo
+function randomZalo() {
+    var zalo = [
+        'https://zalo.me/g/oqmxur261'
+    ]
+    var i = parseInt(Math.random() * zalo.length);
+    location.href = zalo[i];
+}
+
